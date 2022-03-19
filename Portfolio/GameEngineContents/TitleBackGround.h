f0ca5eed12a14a16c52b46f52f10d47916d53db6
장중혁 <wndgur98@gmail.com>
2022-03-19 21:23:31 +0900

    장중혁 : 정리

--- Portfolio/GameEngineContents/TitleBackGround.h
+++ Portfolio/GameEngineContents/TitleBackGround.h
@@ -5,11 +5,9 @@
 class TitleBackGround : public GameEngineActor
 {
 public:
-	// constrcuter destructer
 	TitleBackGround();
 	~TitleBackGround();
 
-	// delete Function
 	TitleBackGround(const TitleBackGround& _Other) = delete;
 	TitleBackGround(TitleBackGround&& _Other) noexcept = delete;
 	TitleBackGround& operator=(const TitleBackGround& _Other) = delete;
